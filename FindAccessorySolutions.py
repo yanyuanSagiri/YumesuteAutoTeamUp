43f574dc9a373ce93ee90bbd532402586e8ca967
unknown <1343891195@qq.com>
2026-06-02 14:50:55 +0800

    docs: Add comment for unfinished files

---
+++ FindAccessorySolutions.py
@@ -0,0 +1,5 @@
+"""
+FindAccessorySolution module is used to prune some accessory' status, and combinate with previous C/P status.
+Input: status of characters and posters.
+Output: status of characters, posters, and accessories.
+"""
---
+++ FindCharacterSolutions.py
@@ -0,0 +1,8 @@
+"""
+FindPosterSolution module is used to prune some characters' status in preliminary preprocessing phase.
+Input: relative files' path.
+Output: preliminary character with useful characters.
+        Characters are maintained by a DAG which records the dominating relations.
+
+But it seems not essential to do. Characters will be picked by user currently.
+"""
